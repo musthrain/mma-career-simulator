a04404b47e107ac34e0f5bae8a7f290ef45c876f
musthrain <74981329+musthrain@users.noreply.github.com>
2025-04-08 17:44:53 +0300

    MmaCareerApplication.java

---
+++ src/main/java/com/mmacareer/MmaCareerApplication.java
@@ -0,0 +1,11 @@
+package com.mmacareer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmaCareerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MmaCareerApplication.class, args);
+    }
+}
